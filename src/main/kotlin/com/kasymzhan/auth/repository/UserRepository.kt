030c4990a73e8b1b5aecbb1bce7de468e78cdb23
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-10 13:53:20 +0900

    fixed id reference

--- src/main/kotlin/com/kasymzhan/auth/repository/UserRepository.kt
+++ src/main/kotlin/com/kasymzhan/auth/repository/UserRepository.kt
@@ -1,10 +1,11 @@
 package com.kasymzhan.auth.repository
 
 import com.kasymzhan.auth.data.User
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : MongoRepository<User, Long> {
+interface UserRepository : MongoRepository<User, ObjectId> {
     fun findByName(username: String): User?
 }
